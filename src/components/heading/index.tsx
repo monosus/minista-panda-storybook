474b823e60a2d97b2894c5ce2c1eb293ce2dfd98
monoharada <harada@monosus.co.jp>
2023-08-16 14:08:36 +0900

    chore(lib): install ts-pattern

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,36 +1,53 @@
-import React from 'react'
-import { css } from '@styled-system/css'
-import { center } from '@styled-system/patterns'
+import React from 'react';
+import { css } from '@styled-system/css';
+import { center, circle } from '@styled-system/patterns';
+import { match } from 'ts-pattern';
 
-type AllowedTags = Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'h3'>
+type AllowedTags = Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'h3'>;
 
 interface Props {
-  children: React.ReactNode
-  as?: keyof AllowedTags
+  children: React.ReactNode;
+  as?: keyof AllowedTags;
 }
-const Heading = ({ children, as = 'h2' }: Props) => {
-  const Tag = as // タグ名を動的に決定するための変数
+const Heading = ({ children, as = 'h2' }: Props): JSX.Element => {
+  const Tag = as; // タグ名を動的に決定するための変数
   return (
+    <>
+      <span>
+        {match(as)
+          .with('h3', () => 'h3だよ')
+          .with('h1', () => 'h1だよ')
+          .with('h2', () => 'h2だよ')
+          .exhaustive()}
+      </span>
+      <div
+        className={match(as)
+          .with('h1', () => center({ bg: 'tomato', aspectRatio: 1, flexWrap: 'wrap', gap: '0 300px' }))
+          .with('h2', () => circle({ bg: 'navy' }))
+          .with('h3', () => circle({ bg: 'banana' }))
+          .exhaustive()}
+      >
+        <Tag
+          className={css({
+            color: 'danger',
+            fontWeight: 'bold',
+            sm: { fontSize: '40px' },
+          })}
+        >
+          {children}
+        </Tag>
+        <Tag
+          className={css({
+            color: 'danger',
+            fontWeight: 'bold',
+            sm: { fontSize: '40px' },
+          })}
+        >
+          {children}
+        </Tag>
+      </div>
+    </>
+  );
+};
 
-    <div className={[
-      // stack({ direction: 'row', paddingY: '10' }),
-      center({ bg: 'tomato', aspectRatio: 1, flexWrap: 'wrap', gap: '0 300px' })
-    ].join('')}>
-      <Tag className={
-        css({
-          color: 'danger',
-          fontWeight: 'bold',
-          sm: { fontSize: '40px' }
-        })}>{children}</Tag>
-      <Tag className={
-        css({
-          color: 'danger',
-          fontWeight: 'bold',
-          sm: { fontSize: '40px' }
-        })}>{children}</Tag>
-    </div>
-
-  )
-}
-
-export default Heading
+export default Heading;
