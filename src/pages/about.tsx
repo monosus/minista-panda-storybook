abd48c22cf7e400311aa3385653c80553f09eafa
monoharada <harada@monosus.co.jp>
2023-08-19 10:32:59 +0900

    test(pages,assets): デモに適用した

---
+++ src/pages/about.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { css } from '@styled-system/css';
+import HtmlAppendedLink from '~/components/util/HtmlAppendedLink';
+import { Head, Image } from 'minista';
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>about</title>
+        <script type="module" src="/src/assets/entry.ts" />
+      </Head>
+      <div className={css({ border: '4px dotted orange', width: '5/12' })}>hoge</div>
+      <nav hx-boost="true">
+        <HtmlAppendedLink href="/" baseDir="">
+          home?
+        </HtmlAppendedLink>
+        <a href="/">home</a>
+      </nav>
+      <HtmlAppendedLink href="/">no boost home?</HtmlAppendedLink>
+      <figure className={css({ w: '1/3' })}>
+        <Image
+          src="/src/assets/images/jainam-sheth-SawbRerFzXs-unsplash.jpg"
+          format="webp"
+          quality={65}
+          loading="eager"
+        ></Image>
+      </figure>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import type { PageProps, Metadata } from 'minista';
+import type { Metadata } from 'minista';
 import { Head } from 'minista';
 import * as Components from '~/components';
 import { Button, heading as Heading } from '~/components';
+import HtmlAppendedLink from '~/components/util/HtmlAppendedLink';
 
 export const metadata: Metadata = {
   title: 'Home',
   page: 'hoge',
 };
-const index = ({ url, title, children }: PageProps) => {
+const index = () => {
   return (
     <>
       <Head>
         <script type="module" src="/src/assets/entry.ts" />
       </Head>
+      <nav hx-boost="true">
+        <HtmlAppendedLink href="/about" className="hoge">
+          about.html?
+        </HtmlAppendedLink>
+        <HtmlAppendedLink href="/about" className="hoge" baseDir="/other">
+          other about.html?
+        </HtmlAppendedLink>
+        <a href="/about">about</a>
+      </nav>
+      <HtmlAppendedLink href="/about" className="hoge">
+        no boost
+      </HtmlAppendedLink>
       <div>Enter</div>
       <Button size="sm" visual="solid">
         button
