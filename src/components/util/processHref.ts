727f0ef8b26ce8600ad043918d5750cb89645648
monoharada <harada@monosus.co.jp>
2023-08-27 15:33:58 +0900

    add:processHref method

---
+++ src/components/util/processHref.ts
@@ -0,0 +1,19 @@
+type ProcessHrefOptions = {
+    href: string;
+    shouldAppendHtml: boolean;
+    isProd?: boolean;
+  };
+  
+  function processHref(options: ProcessHrefOptions): string {
+    const { href, shouldAppendHtml, isProd = import.meta.env.PROD } = options;
+    let processedHref = href;
+  
+    if (isProd && shouldAppendHtml && !href.endsWith('/')) {
+      processedHref += '.html';
+    }
+  
+    return processedHref;
+  }
+  
+  export default processHref;
+  
