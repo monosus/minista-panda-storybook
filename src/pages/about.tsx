4821ccc2be8d6a74bb032e72d07cb0115b1a108f
monoharada <harada@monosus.co.jp>
2023-09-10 18:24:14 +0900

    fix about.tsx meta.ts

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { css } from '@styled-system/css';
 import HtmlAppendedLink from '~/components/util/HtmlAppendedLink';
-import { Head, Image } from 'minista';
+import { Head, Image, type Metadata } from 'minista';
 import { hstack } from '@styled-system/patterns';
+
+export const metadata: Metadata = {
+  title: 'about',
+  page: 'about hoge',
+};
 export default function About() {
   return (
     <>
@@ -13,14 +18,11 @@ export default function About() {
       <div className={css({ border: '4px dotted orange', width: '5/12' })}>
         <HtmlAppendedLink href="hoge">abbbb</HtmlAppendedLink>
       </div>
-      <nav>
-        <HtmlAppendedLink href="/" baseDir="">
-          home?
-        </HtmlAppendedLink>
+      <nav hx-boost="true">
+        <HtmlAppendedLink href="/">home?</HtmlAppendedLink>
       </nav>
       <StackImage />
       <HtmlAppendedLink href="/">no boost home?</HtmlAppendedLink>
-      <script>console.log(page)</script>
       <script type="module" src="/src/assets/about.ts" />
     </>
   );
